562fe9a5f32d494d28b31d853a25cf1d69acd712
Robin Galema <RGalwma99@gmail.com>
2020-04-15 14:00:45 +0200

    Added basic structure for Johnny Five

--- Prototypes/Technisch/Arduino POC/public/app.js
+++ Prototypes/Technisch/Arduino POC/public/app.js
@@ -1,1 +1,27 @@
 var socket = io();
+
+const checkForBoards = () =>
+{
+    socket.emit('boardQuery');
+}
+
+socket.on('boardInfo', (boardInfo) =>
+{
+    console.log(boardInfo);
+    if (boardInfo.isConnected)
+    {
+        console.log(`The Board is connected`);
+        if (boardInfo.isReady)
+        {
+            console.log(`   --> The board is ready for usage`);
+        }
+        else
+        {
+            console.error('   --> The board is not ready for usage');
+        }
+    }
+    else
+    {
+        console.error("There is no board connected");
+    }
+})
--- Prototypes/Technisch/Arduino POC/server.js
+++ Prototypes/Technisch/Arduino POC/server.js
@@ -1,8 +1,14 @@
+// Express and Socket.io
 const express = require('express');
 const app = express();
 var http = require('http').createServer(app);
 const io = require("socket.io")(http);
 var publicDir = require('path').join(__dirname,'/public');
+
+// Johnny Five
+const { Board, Proximity } = require("johnny-five");
+const board = new Board();
+
 
 // Set the page to index.html
 app.get('/', function(req, res){
@@ -18,4 +24,12 @@ http.listen(3000, function(){
 io.on('connection', (socket) => 
 {
     console.log('a user connected');
+
+    socket.on("boardQuery", () =>
+    {
+        console.log("--> Client board query");
+        let connectedBoard = board;
+        console.log("   --> Sending board info to client");
+        io.emit('boardInfo', connectedBoard);
+    })
 })
