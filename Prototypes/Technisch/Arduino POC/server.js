8a777a58626456c14562567f39d7bf9435ea0dc2
Robin Galema <RGalwma99@gmail.com>
2020-04-15 13:38:17 +0200

    Adding socket IO

--- Prototypes/Technisch/Arduino POC/server.js
+++ Prototypes/Technisch/Arduino POC/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+var http = require('http').createServer(app);
+const io = require("socket.io")(http);
+var publicDir = require('path').join(__dirname,'/public');
 
-app.use(express.static('public'));
+// Set the page to index.html
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
-});
+app.use(express.static(publicDir));
 
-app.listen(3000, () => console.log('listening on port 3000!'));
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+io.on('connection', (socket) => 
+{
+    console.log('a user connected');
+})
