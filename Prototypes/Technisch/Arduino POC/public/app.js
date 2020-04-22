4679ba0c0c4d2ce65a701ef418cd2968801fbb96
Robin Galema <RGalwma99@gmail.com>
2020-04-22 11:42:46 +0200

    Function for range detection and commenting

--- Prototypes/Technisch/Arduino POC/public/app.js
+++ Prototypes/Technisch/Arduino POC/public/app.js
@@ -1,11 +1,21 @@
+// Global Variables
 var socket = io();
 
+
+//  --- Socket IO ---
+socket.on('boardInfo', (boardInfo) =>
+{
+    console.log(`--> Is the board connected and ready? ${boardInfo}`);
+})
+
+
+// --- Functions ---
+
+/**
+ * A function that sends a request to the server to get information about the connected Arduino board
+ */
 const checkForBoards = () =>
 {
     socket.emit('boardQuery');
 }
 
-socket.on('boardInfo', (boardInfo) =>
-{
-    console.log(`--> Is the board connected and ready? ${boardInfo}`);
-})
--- Prototypes/Technisch/Arduino POC/server.js
+++ Prototypes/Technisch/Arduino POC/server.js
@@ -9,7 +9,11 @@ var publicDir = require('path').join(__dirname,'/public');
 const { Board, Proximity } = require("johnny-five");
 const board = new Board();
 
+// Global variables
 let isSomebodyInRange = false;
+
+
+//  --- Page Loading ---
 
 // Set the page to index.html
 app.get('/', function(req, res){
@@ -22,11 +26,16 @@ http.listen(3000, function(){
     console.log('listening on *:3000');
   });
 
+
+//  --- Socket IO ---
+
+// Function for when a user loads the webpage connected to this server
 io.on('connection', (socket) => 
 {
     console.log('a user connected');
     console.log(board);
 
+    // Function for when the user requests information about the connected board
     socket.on("boardQuery", () =>
     {
         console.log("--> Client board query");
@@ -37,23 +46,42 @@ io.on('connection', (socket) =>
 });
 
 
+//  --- Arduino board functions (Johnny Five) ---
+
+// Function for when the Arduino board is loaded and ready to use
 board.on("ready", () => {
   const proximity = new Proximity({
-    controller: "HCSR04",
-    pin: 7
+    controller: "HCSR04", // Type of the connected sensor
+    pin: 7 // To which pin the sensor is connected
   });
 
+  // Code that runs when the value returned by the proximity sensor changes
   proximity.on("change", () => {
     const {centimeters, inches} = proximity;
-    if (centimeters <= 30 && !isSomebodyInRange)
-    {
-        console.log("Iemand is in range gekomen");
-        isSomebodyInRange = true;
-    }
-    else if (centimeters > 30 && isSomebodyInRange)
-    {
-      console.log("Iemand is uit de range gelopen");
-      isSomebodyInRange = false;
-    }
+    checkForRange(centimeters);
   });
 });
+
+
+
+//  --- Functions ---
+
+/**
+ * A function that checks if there is an object in range of the sensor
+ * 
+ * @param {Number} centimeters The amount of centimeters the nearest object is to the sensor, given by the Arduino sensor in the proximity.on function
+ * @param {Number} range The range at which an object is considered in range
+ */
+const checkForRange = (centimeters, range = 30) =>
+{
+  if (centimeters <= range && !isSomebodyInRange)
+  {
+      console.log("|--> Something came in range of the sensor");
+      isSomebodyInRange = true;
+  }
+  else if (centimeters > range && isSomebodyInRange)
+  {
+    console.log("     |---> Something left the range of the sensor");
+    isSomebodyInRange = false;
+  }
+}
