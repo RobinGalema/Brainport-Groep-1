c3acb917f6efada3d2d45e311bf2fad957d7b378
Thomas <393890@student.fontys.nl>
2020-05-11 10:58:09 +0200

    map added

--- Prototypes/Technisch/Map/app.js
+++ Prototypes/Technisch/Map/app.js
@@ -1,4 +1,7 @@
-const initMap = () =>
-{
-
+function initMap() {
+  var uluru = { lat: 51.441642, lng: 5.4697225 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: uluru, zoom: 12,
+  });
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
