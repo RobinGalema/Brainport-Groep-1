ea441029a2401616de163776f38044281051f428
Thomas <393890@student.fontys.nl>
2020-05-11 14:39:07 +0200

    loads json lat and long to markers

--- Prototypes/Technisch/Map/app.js
+++ Prototypes/Technisch/Map/app.js
@@ -1,7 +1,64 @@
-function initMap() {
-  var uluru = { lat: 51.441642, lng: 5.4697225 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: uluru, zoom: 12,
+let jsonData;
+let markers = [];
+/**
+ * loads json, executes function, dabs on haters
+ * @param {string} path path to json file
+ * @param {function} callback DIT IS EEN CALLBACK
+ */
+const fetchJSONFile = (path, callback) => {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open("GET", path);
+  httpRequest.send();
+};
+
+window.onload = () => {
+  fetchJSONFile("data.json", function (data) {
+    jsonData = data;
+    console.log(data);
+    MakeMap();
+    CreateMarkers();
   });
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+};
+
+/**
+ * executes when api is loaded
+ */
+var initMap = () => {
+  console.log("maps API loaded.");
+};
+
+/**
+ * creates map and marker
+ */
+const MakeMap = () => {
+  var uluru = {
+    lat: jsonData.companies[0].marker.lat,
+    lng: jsonData.companies[0].marker.long,
+  };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: uluru,
+    zoom: 12,
+  });
+};
+
+/**
+ * creates makers and pushes them to markers array
+ */
+const CreateMarkers = () => {
+  jsonData.companies.forEach((element) => {
+    var uluru = {
+      lat: element.marker.lat,
+      lng: element.marker.long,
+    };
+    let marker = new google.maps.Marker({ position: uluru, map: map });
+    markers.push(marker);
+  });
+};
