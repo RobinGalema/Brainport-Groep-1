0f99327ae7067089ca5b3d303f9aaea5bea36b86
Charly Vos <charlyvos@hotmail.com>
2020-05-12 14:27:51 +0200

    updated to es6 & made switch instead of if else

--- Prototypes/Technisch/Hologram socket prototype/server.js
+++ Prototypes/Technisch/Hologram socket prototype/server.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var app = express();
-var server = app.listen(3000);
+const express = require('express');
+const app = express();
+const server = app.listen(3000);
 
 app.use(express.static('public'));
 
 console.log("My socket server is live!");
 
-var socket = require('socket.io');
-var io = socket(server);
+const socket = require('socket.io');
+const io = socket(server);
 
 io.sockets.on('connection', newConnection)
 
 function newConnection(socket){
     console.log(`new connection: ${socket.id}`);
 
-    socket.on('commando', function(nummer) {
+    socket.on('commando', nummer => {
 
-        console.log("socket1 ontvangt iets en verstuurt " + nummer);
+        console.log(`socket1 ontvangt iets en verstuurt ${nummer}`);
 
-        //stuur data naar tablet.html
+        //stuur data naar index.html
         io.emit("commando", nummer);
     })
 }
+
