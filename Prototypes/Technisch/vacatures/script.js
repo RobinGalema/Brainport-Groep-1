c801dde3b6d57a536b7ba99197a308418be5557e
Charly Vos <charlyvos@hotmail.com>
2020-06-09 09:50:36 +0200

    Show data from json

--- Prototypes/Technisch/vacatures/script.js
+++ Prototypes/Technisch/vacatures/script.js
@@ -2,11 +2,17 @@ let submit = document.getElementById('submitSearch');
 submit.addEventListener("click", searchJobOffer);
 
 function searchJobOffer() {
+  let htmlString = "";
   let userInput = document.getElementById('inputSearch').value;
 
   fetch('data.json')
   .then((res) => res.json()) //format JSON
   .then((json) => json.companies.forEach(element => {
-    if (element.city == userInput) console.log(element.joboffer);
-  }))
+    if (element.city == userInput) {
+      element.joboffer.forEach(element => {
+        console.log(element);
+        htmlString += "<div class='vacature'><div class='info'><h2>" + element.title +  "</h2><p>" + element.description + "</p> <p>Salary: " + element.salary + "</p></div></div>";
+        $('#output').html(htmlString);
+      });
+  }}))
 }
