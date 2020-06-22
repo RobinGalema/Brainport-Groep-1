e8477c680bf715c2419e3002ccdb03bb360008e7
Charly Vos <charlyvos@hotmail.com>
2020-06-22 16:24:33 +0200

    add jsdoc

--- Prototypes/Technisch/vacatures/resources/js/lib/Searchjoboffer.js
+++ Prototypes/Technisch/vacatures/resources/js/lib/Searchjoboffer.js
@@ -2,30 +2,46 @@ const Searchjoboffer = (function () {
     let submitElement;
     let htmlString;
     let userInput;
+    let data;
 
-    const init = (submitElementId = "submitSearch", inputSearchValue = "inputSearch", outputId = "output") => {
+    fetch('resources/data/data.json')
+    .then((res) => res.json())
+    .then((json) => data = json)
+
+    /**
+     * Collects the id's from the form and which div to output the job offers to
+     * @param  {string} submitElementId="submitSearch" the submit button ID from the form
+     * @param  {string} inputSearchValue="inputSearch" the input id from the form.
+     * @param  {string} outputId="output" the output id to show job offers.
+     */
+    const init = (submitElementId = "submitSearch", inputSearchValue = "inputSearch", outputId = "output") => {   
         submitElement = document.getElementById(submitElementId);
         submitElement.addEventListener("click", () => {
-          userInput = document.getElementById(inputSearchValue).value; //your input field id
+          userInput = document.getElementById(inputSearchValue).value;
           searchJobOffer(userInput);
         });
-        outputElement = document.getElementById(outputId); //your output container id
+        outputElement = document.getElementById(outputId);
     }
-
+    
+    /**
+     * This function will reset the output, then search the JSON file for any matching 
+     * job offers found within the paramater
+     * 
+     * @param  {string} userInputValue the user input from the search form (a city)
+     * @return Outputs the found job offers in the output div
+     */
     const searchJobOffer = async (userInputValue) => {
-        htmlString = "";   //reset the output string empty
+        htmlString = "";
       
-        await fetch('resources/data/data.json') //get data from json
-        .then((res) => res.json()) //format JSON
-        .then((json) => json.companies.forEach(element => { //for each company in the json >
-          if (element.city == userInputValue) { //Check which company match with the user input
+        data.companies.forEach(element => { 
+          if (element.city == userInputValue) {
             if (element.joboffer) {
-              element.joboffer.forEach(element => { //for each matched company > job offers > 
+              element.joboffer.forEach(element => {
                 htmlString += `<div class='jobOffer'><div class='info'><h2>${element.title}</h2><p>${element.description}</p> <p>Salary: ${element.salary}</p></div></div>`; // set the string with data from json
-                $(outputElement).html(htmlString); //show the output on the front-end              
+                $(outputElement).html(htmlString);         
               });
             }
-        }}))
+        }})
       }
 
     return {init}
